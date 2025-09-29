f307750c35386a448160b08bb0eab350c23dacce
newbe01 <pkwaa3@gmail.com>
2025-09-29 21:14:34 +0900

    tt

---
+++ src/main/java/com/example/testpj/TestpjApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpj/TestpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
